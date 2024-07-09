162ab6e9de06b2aa8fa54dbb69ad882c4acdd3ff
Hassaan <ssc.hassaan.190224@gmail.com>
2024-07-09 16:41:46 +0500

    Master7

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middlewares
-app.use(cors({
-    origin: 'https://e-commerce-website-client-woad.vercel.app/', // or '*' to allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}))
+app.use(cors())
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.json())
@@ -50,4 +47,5 @@ const server = app.listen(PORT, () => {
     console.log(`Server running on: ${PORT}`)
 })
 
-export default app
+// export default app
+export { server, app }
