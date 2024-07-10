cf5bff316c645b7d5dddc0f1075a27d806858143
Hassaan <ssc.hassaan.190224@gmail.com>
2024-07-10 11:13:24 +0500

    methods

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const corsOptions = {
     origin: 'https://e-commerce-website-client-woad.vercel.app',
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
     allowedHeaders: 'Content-Type,Authorization'
 }
   
